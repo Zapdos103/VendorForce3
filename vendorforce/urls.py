9b7838ad6e2c6df02b15812f174cea02e49add2f
Kenzo <kenzoft20@gmail.com>
2024-04-29 05:30:07 -0300

    .

--- vendorforce/urls.py
+++ vendorforce/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('empresas.urls')),
     path('painel/', include('painel.urls')),
     path('formularios2/', include('formularios2.urls')),
+    path('', lambda request: redirect('/auth/cadastro_completo')),
 ]
