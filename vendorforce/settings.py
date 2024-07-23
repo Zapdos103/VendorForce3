4f0979b3601337575853890521a7d7b70bb69fdb
Kenzo <kenzoft20@gmail.com>
2024-07-23 15:26:51 -0300

    allowed hosts atualizado

--- vendorforce/settings.py
+++ vendorforce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=False)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['185.139.1.238']
 
 
 # Application definition
