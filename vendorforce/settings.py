22f22671d88096e93bce04cc7cf9d2be9d4c4485
Kenzo <kenzoft20@gmail.com>
2024-09-13 14:55:55 -0300

    teste

--- vendorforce/settings.py
+++ vendorforce/settings.py
@@ -86,13 +86,29 @@ WSGI_APPLICATION = 'vendorforce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'assessment_db',
+        'USER': 'kenzo',
+        'PASSWORD': 'sinsalab1M_$',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+        'charset': 'utf8mb4',
+        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
