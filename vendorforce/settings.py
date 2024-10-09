d5578397738eb36670f3f10c878285dd82980a99
Kenzo <kenzoft20@gmail.com>
2024-10-09 10:21:29 -0300

    .

--- vendorforce/settings.py
+++ vendorforce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=False)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
